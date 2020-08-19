6b1b7b3b8dfd7dfdbe853d315b9a3a51cbf4cda8
Ant C <anthonyrobertcook@gmail.com>
2020-08-19 12:51:37 +0100

    Add project files.

---
+++ MVC-Core-WebApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MVC_Core_WebApplication.Data;
+
+[assembly: HostingStartup(typeof(MVC_Core_WebApplication.Areas.Identity.IdentityHostingStartup))]
+namespace MVC_Core_WebApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
